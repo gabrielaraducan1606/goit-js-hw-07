8509e2fcceb55048b3f7a980ac934e5b93c13d67
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2024-08-14 00:18:49 +0300

    commit1

---
+++ js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const createGalleryMarkup = (items) => {
+    return items.map(({ preview, original, description }) => {
+        return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+    }).join("");
+};
+    
+const renderGallery = () => {
+   galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+};
+renderGallery();
+
+const lightBox = new SimpleLightbox('.gallery a', {
+   captionsData: 'alt',
+   captionDelay: 250.
+});
